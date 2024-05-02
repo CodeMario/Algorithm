3d017b4009bf687e6841b409117d7e99583085c4
CodeMario <167953745+CodeMario@users.noreply.github.com>
2024-05-02 23:38:39 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,11 @@
+def solution(price):
+    answer = 0
+    discount = 1
+    if price >= 500000 :
+        discount = 0.8
+    elif price >= 300000 :
+        discount = 0.9
+    elif price >= 100000 :
+        discount = 0.95
+    answer = int(price*discount)
+    return answer
