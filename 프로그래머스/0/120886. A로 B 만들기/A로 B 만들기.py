f1bbf5acc97e57ff7a142e1d757e9896091b9934
CodeMario <167953745+CodeMario@users.noreply.github.com>
2024-05-04 01:38:20 +0900

    [level 0] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -0,0 +1,9 @@
+def solution(before, after):
+    answer = 0
+    before = list(before)
+    after = list(after)
+    before.sort()
+    after.sort()
+    if before == after :
+        answer = 1
+    return answer
