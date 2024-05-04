9a2149e01c1c219d677a003028670f3560f6a467
CodeMario <167953745+CodeMario@users.noreply.github.com>
2024-05-04 13:46:28 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    answer = ''
+    acc = ''
+    change = {"zero":'0', "one":'1', "two":'2', 
+              "three":'3', "four":'4', "five":'5',
+              "six":'6', "seven":'7', "eight":'8', "nine":'9'}
+    for i in numbers :
+        acc += i
+        if acc in change :
+            answer += change[acc]
+            acc = ''
+    answer = int(answer)
+    return answer
