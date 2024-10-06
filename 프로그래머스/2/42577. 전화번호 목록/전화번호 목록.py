c6dde4b080df09284240566883fd4762bb309891
CodeMario <167953745+CodeMario@users.noreply.github.com>
2024-10-06 12:40:28 +0900

    [level 2] Title: 전화번호 목록, Time: 99.68 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,10 @@
 def solution(phone_book):
     answer = True
-    if len(phone_book) != 1:
-        phone_book.sort(key=lambda x : x)
-        for i in range(len(phone_book)-1):
-            if phone_book[i] in phone_book[i+1][:len(phone_book[i])]:
-                answer = False
-                break
+    phone_book.sort()
+    
+    for i in range(len(phone_book)-1) :
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])] :
+            answer = False
+            break
+    
     return answer
